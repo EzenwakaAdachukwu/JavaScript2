8b023bc4ef1c73ea7e3b13ee7b5d48b917a0f891
EzenwakaAdachukwu <105318332+EzenwakaAdachukwu@users.noreply.github.com>
2022-06-23 10:31:52 -0700

    Zuri JS assignment

---
+++ calculator.js
@@ -0,0 +1,26 @@
+// number1  =;
+// number2 = ;
+
+const number1 = parseFloat(prompt("Enter first number: "));
+
+const operator = prompt("Enter operator (either +, -, * or / ):");
+
+const number2 = parseFloat(prompt("Enter second number: ")); 
+
+let result;
+
+if (operator == "+") {
+    result = number1 + number2
+}
+else if (operator == "-") {
+    result = number1 - number2
+}
+else if (operator == "*") {
+    result = number1 - number2
+}
+else{
+    result = number1 / number2
+}
+
+alert (result)
+// ("${number1} ${operator} ${number2} = ${result}")
